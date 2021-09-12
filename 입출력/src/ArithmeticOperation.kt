a4eb4a50174667999ddc42cec582492fbacf5144
cjy2103 <cjy2103@naver.com>
2021-09-12 09:05:49 +0900

    코틀린 입출력 테스트

---
+++ 입출력/src/ArithmeticOperation.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>){
+    var num = 10
+    var num2 = 6
+
+    println("µ¡¼À : $num + $num2 = ${num+num2}")
+    println("»¬¼À : $num - $num2 = ${num-num2}")
+    println("°ö¼À : $num * $num2 = ${num*num2}")
+    println("³ª´©±â : $num / $num2 = ${num/num2}")
+    println("³ª¸ÓÁö : $num % $num2 = ${num%num2}")
+
+    var str = "¹®ÀÚ¿­ ÀÌ¾î¾²±â ¼º°ø"
+    println("¹®ÀÚ¿­ ÀÌ¾î¾²±â : $str")
+
+
+}
