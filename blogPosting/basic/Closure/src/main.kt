8522dd307c2774fb292c6656cfdeeaec4f867eb9
jy.choi <cjy2103@naver.com>
2022-08-09 14:53:14 +0900

    코틀린 클로저 포스팅 완

--- blogPosting/basic/Closure/src/main.kt
+++ blogPosting/basic/Closure/src/main.kt
@@ -1,3 +1,16 @@
+
+
 fun main() {
-    println("Test")
+    val num = Number()
+    var result = 0;
+    num.add(3,5){
+        x,y -> result = x+y
+    }
+    println(result)
+}
+
+class Number{
+    fun add(a: Int, b: Int, add : (Int, Int) -> Unit){
+        add(a, b)
+    }
 }
