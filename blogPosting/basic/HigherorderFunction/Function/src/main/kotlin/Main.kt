fbc7cb1c694c999012b6ceb80cd27bc94d923f1f
jy.choi <cjy2103@naver.com>
2023-01-09 18:00:15 +0900

    고차함수 포스팅중

--- blogPosting/basic/HigherorderFunction/Function/src/main/kotlin/Main.kt
+++ blogPosting/basic/HigherorderFunction/Function/src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//    val sum : (Int, Int) -> Int = {x, y -> x+y}
+    val sum = {x: Int , y : Int -> x+y}
+    val a = sum(2,3)
+    print(a)
 }
+
+
