8de4a8c079f2a43c28ba132d3d93ca78e6cf33da
jy.choi <cjy2103@naver.com>
2023-01-10 11:24:49 +0900

    고차함수 포스팅중

--- blogPosting/basic/HigherorderFunction/Function/src/main/kotlin/Main.kt
+++ blogPosting/basic/HigherorderFunction/Function/src/main/kotlin/Main.kt
@@ -1,9 +1,25 @@
 fun main(args: Array<String>) {
-
 //    val sum : (Int, Int) -> Int = {x, y -> x+y}
-    val sum = {x: Int , y : Int -> x+y}
-    val a = sum(2,3)
-    print(a)
+//    val sum = {x: Int , y : Int -> x+y}
+//    val a = sum(2,3)
+    //    print(a)
+    plus(2,3, add = {
+        a,b -> a+b
+    })
+
+    minus(10,5, subtract = {
+        a,b -> a-b
+        print("$a - $b -> ${a-b}")
+    })
+
 }
 
+fun plus(a : Int, b : Int, add : (Int, Int) -> Int ){
+    println("$a, $b -> ${add(a,b)}")
+}
 
+fun minus(a : Int, b : Int, subtract : (Int, Int) -> Unit){
+    subtract(a,b)
+}
+
+//println("$a - $b -> ${a-b}")
