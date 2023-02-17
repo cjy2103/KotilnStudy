067dc645418184398dccea73f31331d08cad5f7f
jy.choi <cjy2103@naver.com>
2023-02-17 11:15:40 +0900

    코틀린 String 값 비교 포스팅 완

---
+++ blogPosting/basic/Compare/Compare/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val str = String("test".toByteArray(Charsets.UTF_8))
+    val test = "test"
+    val test2 = "test";
+
+    println(str==test)
+    println(str===test)
+    println(test===test2)
+
+
+
+
+}
