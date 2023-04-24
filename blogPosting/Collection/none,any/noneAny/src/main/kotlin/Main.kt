a9115ac653f293783b76208e76813dc767b6d9d0
jy.choi <cjy2103@naver.com>
2023-04-24 20:07:08 +0900

    코틀린 none, any 포스팅 완

--- blogPosting/Collection/none,any/noneAny/src/main/kotlin/Main.kt
+++ blogPosting/Collection/none,any/noneAny/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val numbers = listOf(1,2,3,4,5)
+//    val result = numbers.none{it > 10}
+//    println(result)
+    val result = numbers.any { it > 3 }
+    println(result)
 }
