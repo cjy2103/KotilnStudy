b0df8c41b16fbced7d0f7a94edb4724116eb4cf9
jy.choi <cjy2103@naver.com>
2023-07-31 08:15:27 +0900

    sealed class 포스팅 완

--- blogPosting/basic/SealedClass/SealedClass/src/main/kotlin/Main.kt
+++ blogPosting/basic/SealedClass/SealedClass/src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val shapes: List<Shape> = listOf(
+        Circle(5.0),
+        Square(4.0),
+        Rectangle(3.0, 6.0),
+        Triangle(4.0, 7.0)
+    )
+
+    shapes.forEach { shape ->
+        val area = when(shape){
+            is Circle -> calculateCircleArea(shape.radius)
+            is Square -> calculateSquareArea(shape.side)
+            is Rectangle -> calculateRectangleArea(shape.width, shape.height)
+            is Triangle -> calculateTriangleArea(shape.base, shape.height)
+        }
+        println("Area of ${shape.javaClass.simpleName}: $area")
+    }
+}
+
+
+fun calculateCircleArea(radius: Double): Double {
+    return 3.14 * radius * radius
+}
+
+fun calculateSquareArea(side: Double): Double {
+    return side * side
+}
+
+fun calculateRectangleArea(width: Double, height: Double): Double {
+    return width * height
+}
+
+fun calculateTriangleArea(base: Double, height: Double): Double {
+    return 0.5 * base * height
 }
--- blogPosting/basic/SealedClass/SealedClass/src/main/kotlin/Shape.kt
+++ blogPosting/basic/SealedClass/SealedClass/src/main/kotlin/Shape.kt
@@ -1,4 +1,7 @@
 sealed class Shape
 
 class Circle(val radius : Double) : Shape()
+class Square(val side: Double) : Shape()
+class Rectangle(val width: Double, val height: Double) : Shape()
+class Triangle(val base: Double, val height: Double) : Shape()
 
